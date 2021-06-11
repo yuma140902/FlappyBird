d7952d463a583f077a1a0949a7e8afb79def6c97
yuma140902 <yuma140902@gmail.com>
2021-06-11 18:16:15 +0900

    余分なコードを削除

--- Assets/script/title/StartButton.cs
+++ Assets/script/title/StartButton.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
-  // Start is called before the first frame update
-  void Start()
-  {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
-
   public void OnClick()
   {
     SceneManager.LoadScene(1);
